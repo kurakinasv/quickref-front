9c2c5ec72f6290ff78f57a66c5535e543a17e740
kurakinasv <svetakurakina2002@gmail.com>
2022-12-16 14:28:42 +0300

    [+] mock images

---
+++ src/img/refs/index.ts
@@ -0,0 +1,12 @@
+const refs = {
+    camp: require('./camp.jpeg'),
+    cat1: require('./cat1.jpg'),
+    cat2: require('./cat2.jpg'),
+    cat3: require('./cat3.jpg'),
+    cat4: require('./cat4.jpg'),
+    pepeChill: require('./pepeChill.jpg'),
+    ThumbUpX: require('./ThumbUpX.png'),
+    tucked: require('./tucked.gif'),
+};
+
+export default refs;
